8b3f5c2326e056f4b2cbc077c50e9437c7f4ebb0
Techlanders Solutions <70830147+techlanders@users.noreply.github.com>
2021-06-24 18:32:14 +0530

    Update LoginServlet.java

--- src/main/java/webapp/LoginServlet.java
+++ src/main/java/webapp/LoginServlet.java
@@ -40,7 +40,7 @@ public class LoginServlet extends HttpServlet {
 		out.println("<title>Techlanders Solutions</title>");
 		out.println("</head>");
 		out.println("<body>");
-		out.println("<h1> Welcome to DevOps Workshop - IBM - Version 4 </h1>");
+		out.println("<h1> Welcome to DevOps Workshop - BT - Version 1 </h1>");
 		out.println("</body>");
 		out.println("</html>");
 
